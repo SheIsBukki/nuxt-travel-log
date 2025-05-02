8825910cceee2db8a3041640b4c03614b518009a
Bukola Ogunleye <bukolaogunleye2011@gmail.com>
2025-05-02 16:24:37 +0100

    Set up eslint and lint-staged (#48)
    * Set up eslint and lint-staged
    
    * Added lint workflow using Github actions
    
    * Set up a tool, act, to use to see how things work locally before commiting

---
+++ eslint.config.mjs
@@ -0,0 +1,72 @@
+import antfu from "@antfu/eslint-config";
+
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  // Your custom configs here
+  antfu({
+    // Type of the project. 'lib' for libraries, the default is 'app'
+    type: "app",
+
+    // Enable stylistic formatting rules
+    // stylistic: true,
+
+    // Or customize the stylistic rules
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: "double", // or 'double'
+      semi: true, // I added this because it is in the tutorial
+    },
+
+    // TypeScript and Vue are autodetected, you can also explicitly enable them:
+    typescript: true,
+    vue: true,
+
+    // I added formatters and rules according to the tutorial.
+    formatters: true,
+    rules: {
+      "vue/max-attributes-per-line": [
+        "error",
+        {
+          singleline: {
+            max: 2,
+          },
+          multiline: {
+            max: 1,
+          },
+        },
+      ],
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          tsconfigRootDir: ".",
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+    },
+
+    // Disable jsonc and yaml support
+    jsonc: false, // This is not in the tutorial but is in the plugin code
+    yaml: false, // This is not in the tutorial but is in the plugin code
+
+    // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+    ignores: [
+      "**/fixtures", // This is not in the tutorial but is in the plugin code
+      ".pnpm-store/**", // I added this because it is in the tutorial
+      "**/migrations/*", // I added this because it is in the tutorial
+      // ...globs
+    ],
+  }),
+);
