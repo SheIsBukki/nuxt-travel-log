a2c086c2130badf337f5d830d6715065031cdce9
sheisbukki <bukolaogunleye2011@gmail.com>
2025-05-02 20:14:52 +0100

    Set up tailwindss, daisyUI, nuxt/icon module, and iconify which I am using with icones

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      View Map <Icon
+        name="tabler:map"
+        style="color: black"
+        size="32"
+      />
+    </button>
   </div>
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import antfu from "@antfu/eslint-config";
 
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
+// TODO: add tailwindcss plugin for auto-sorting class names
+
 export default withNuxt(
   // Your custom configs here
   antfu({
