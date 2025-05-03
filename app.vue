05c16985fbd4d7b47602ca9641011d0ecb4da87a
sheisbukki <bukolaogunleye2011@gmail.com>
2025-05-03 01:51:36 +0100

    created a simple landing page with a navbar and hero section using daisy components; navbar and hero for styling

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon
-        name="tabler:map"
-        style="color: black"
-        size="32"
-      />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavBar />
+    <main class="">
+      <slot />
+    </main>
+  </div>
+</template>
