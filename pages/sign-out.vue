77fa2575ed7ac7b27d5a865d0be2d9cde8106e37
sheisbukki <bukolaogunleye2011@gmail.com>
2025-05-06 16:05:54 +0100

    I forgot to add the sign-out file because I used git add -p command

--- pages/sign-out.vue
+++ pages/sign-out.vue
@@ -8,7 +8,7 @@ onMounted(() => {
 
 <template>
   <div
-    class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4"
+    class="card bg-base-200 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4"
   >
     <span class="loading loading-xl loading-spinner text-primary" />
   </div>
