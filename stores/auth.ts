5b50ecb9c3a35f7b319535f1d2a0dedce385dc24
Bukola Ogunleye <bukolaogunleye2011@gmail.com>
2025-05-06 21:19:31 +0100

    SSR Auth session implemented (#62)

--- stores/auth.ts
+++ stores/auth.ts
@@ -4,13 +4,18 @@ import { createAuthClient } from "better-auth/vue";
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-  const session = authClient.useSession();
-
-  const user = computed(() => session.value.data?.user);
-
-  const loading = computed(
-    () => session.value.isPending || session.value.isRefetching,
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(
+    null,
   );
+
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+
+  const user = computed(() => session.value?.data?.user);
+
+  const loading = computed(() => session.value?.isPending);
 
   async function signIn() {
     await authClient.signIn.social({
@@ -25,5 +30,5 @@ export const useAuthStore = defineStore("useAuthStore", () => {
     navigateTo("/");
   }
 
-  return { loading, signIn, signOut, user };
+  return { init, loading, signIn, signOut, user };
 });
