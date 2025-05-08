5f809dbc560075dffb280df5f24a14baf9fad4da
sheisbukki <bukolaogunleye2011@gmail.com>
2025-05-08 22:37:23 +0100

    Created UI to list all the locations user created in the dashboard sidebar

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
 const isSidebarOpen = ref(true);
+const route = useRoute();
+const sidebarStore = useSidebarStore();
+const locationsStore = useLocationStore();
 
 onMounted(() => {
   isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+
+  if (route.path !== "/dashboard") {
+    locationsStore.refresh();
+  }
 });
 
 function toggleSidebar() {
@@ -51,6 +58,36 @@ function toggleSidebar() {
           href="/dashboard/add"
           icon="tabler:circle-plus-filled"
         />
+
+        <div v-if="sidebarStore.loading || sidebarStore.sidebarItems.length" class="divider" />
+        <!-- Loading skeleton -->
+        <div v-if="sidebarStore.loading" class="px-4 ">
+          <div class="skeleton h-4 w-full" />
+        </div>
+
+        <!-- I will use this in production if there's no padding  -->
+        <SidebarButton
+          v-for="item in sidebarStore.sidebarItems"
+          v-else-if="sidebarStore.sidebarItems.length"
+          :key="item.id"
+          :show-label="isSidebarOpen"
+          :label="item.label"
+          :icon="item.icon"
+          :href="item.href"
+        />
+
+        <!-- The instructor thinks we have to wrap this SidebarButton above in a wrapper, using this div element to use the v-else on an element that uses v-for. when he did, there was an additional padding in the location list section so he decided to use a v-if with the negative loading check -->
+        <!-- <div v-if="!sidebarStore.loading && sidebarStore.sidebarItems.length" class="flex flex-col">
+        <SidebarButton
+          v-for="item in sidebarStore.sidebarItems"
+
+          :key="item.id"
+          :show-label="isSidebarOpen"
+          :label="item.label"
+          :icon="item.icon"
+          :href="item.href"
+        />
+        </div> -->
 
         <div class="divider" />
         <SidebarButton
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
-const { data, status } = await useFetch("/api/locations", { lazy: true });
+const locationsStore = useLocationStore();
+const { locations, status } = storeToRefs(locationsStore);
+
+onMounted(() => {
+  locationsStore.refresh();
+});
 </script>
 
 <template>
@@ -12,9 +17,9 @@ const { data, status } = await useFetch("/api/locations", { lazy: true });
       <span class="loading loading-spinner loading-xl text-primary" />
     </div>
 
-    <div v-else-if="data && data.length > 0" class="flex flex-wrap mt-4 gap-2">
+    <div v-else-if="locations && locations.length > 0" class="flex flex-wrap mt-4 gap-2">
       <div
-        v-for="location in data"
+        v-for="location in locations"
         :key="location.id"
         class="card card-compact bg-base-300 h-40 w-72"
       >
