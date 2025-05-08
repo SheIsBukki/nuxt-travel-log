0ee403f8b84971d4c61f289cd71be5182b346569
sheisbukki <bukolaogunleye2011@gmail.com>
2025-05-08 05:05:57 +0100

    Slugs Are URL Friendly and Validated to Be Unique

--- lib/db/schema/auth.ts
+++ lib/db/schema/auth.ts
@@ -18,7 +18,7 @@ export const session = sqliteTable("session", {
   updatedAt: integer().notNull(),
   ipAddress: text(),
   userAgent: text(),
-  userId: text()
+  userId: int()
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
 });
@@ -27,7 +27,7 @@ export const account = sqliteTable("account", {
   id: int().primaryKey({ autoIncrement: true }),
   accountId: text().notNull(),
   providerId: text().notNull(),
-  userId: text()
+  userId: int()
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
   accessToken: text(),
