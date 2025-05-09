30a514c4b29947a51f14dfc0d861d6ee1aa66c64
Bukola Ogunleye <bukolaogunleye2011@gmail.com>
2025-05-09 10:45:47 +0100

    Map setup, with theme modes, default geolocation and zoom (#69)

---
+++ components/app/map.client.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { CENTER_NIGERIA } from "~/lib/constants";
+
+const colorMode = useColorMode();
+// const style = "https://tiles.openfreemap.org/styles/liberty";
+// const style = "/styles/dark.json";
+const style = computed(() => colorMode.value === "dark" ? "/styles/dark.json" : "https://tiles.openfreemap.org/styles/liberty");
+// const center = [-1.559482, 47.21322];
+const zoom = 5;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_NIGERIA"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const CENTER_USA = [-98.5795, 39.8283];
+// I might have written the coordinates in the wrong order
+export const CENTER_NIGERIA = [9.077751, 8.6774567];
+// export const CENTER_NIGERIA = [8.6774567, 9.077751];
